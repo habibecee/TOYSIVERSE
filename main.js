94e77d7063af09b3fb39669519a19a3fde9550d8
”habibecee” <habibe.ce1996@gmail.com>
2022-09-24 17:34:27 +0300

    updated

---
+++ main.js
@@ -0,0 +1,20 @@
+ 
+function gizlegoster() {
+    
+    var basketDiv = document.getElementsByClassName('basket');
+    
+
+    if (basketDiv.style.display == "none") {
+
+        basketDiv.style.display = '';
+       
+
+    } else {
+
+        basketDiv.style.display = 'none';
+        
+
+    }
+
+
+}
