89cf6085d7c7f36489472633bb0c9d0dc7b49262
”habibecee” <habibe.ce1996@gmail.com>
2022-10-02 19:02:54 +0300

    filtered-function-added

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ getToys();
 const createToyStars = (starRate) => {
 	let starRateHtml = "";
 	for (let i = 1; i <= 5; i++) {
-		if (Math.round(starRate) >= i)
+		if (Math.round(starRate) >= i) {
 			starRateHtml += `<span class="fa fa-star checked m-0 p-0"></span>`;
-		else starRateHtml += `<span class="fa fa-star m-0 p-0"></span>`;
+		} else {
+			starRateHtml += `<span class="fa fa-star m-0 p-0"></span>`;
+		}
 	}
 	return starRateHtml;
 };
@@ -27,27 +29,28 @@ const createToyItemsHtml = () => {
 	const toyListEl = document.querySelector(".toy--list");
 	let toyListHtml = "";
 	toyList.forEach((toy, index) => {
-		toyListHtml += `<div class="row col-2 ${
-			index % 2 == 0 && "card"
-		} shadow rounded">
-		<div class="card-img-div col-3">
+		toyListHtml += `
+		
+		<div class="row col-12 ${index % 1 == 0 && "card"} shadow rounded">
+		<div class="card-img-div col-4">
 			<img href='#' src="${toy.imgSource}" class="card-img-top" alt="..."
-				style="width:95%; height:95%;">
+				style="width:100%; height:100%;">
 		</div>
 		<div class="card-div col-8">
 
 			<h4 class="card-title ürün-ad"> ${toy.name} </h4>
 			<h6 class="cart-title "> ${toy.brand} </h6>
 			<span class="m-0 p-0"> ${toy.starRate} </span>
-			${createToyStars()}
+			${createToyStars(toy.starRate)}
 			
 			<span class="m-0 p-0"> (${toy.reviewCount}) </span>
 
 			<div class="card-details-div col ">
 				<p class="card-text m-0">${toy.description} </p>
 				${
-					toy.oldPrice &&
-					`<span class="price"> <del class="text-danger margin-right-3"> ${toy.oldPrice} &#8378 </del> `
+					toy.oldPrice
+						? `<span class="price"> <del class="text-danger margin-right-3"> ${toy.oldPrice} &#8378 </del> `
+						: ""
 				}
 				<b style="font-size: 23px ;"> ${toy.price} &#8378 </b> </span>
 				<button type="button" class="btn btn-a card-btn"><a href="#" class="card-link">Ürün Detayını
@@ -64,6 +67,47 @@ const createToyItemsHtml = () => {
 	toyListEl.innerHTML = toyListHtml;
 };
 
+const TOYS_TYPES = {
+	ALL: "TÜMÜ",
+	BABY: "O-3 YAŞA UYGUN",
+	HANDMADE: "EL YAPIMI OYUNCAKLAR",
+	CHARACTER: "KARAKTER OYUNCAKLARI ",
+	LEGO: "LEGO",
+	PLUSH: "PELUŞ  ",
+	OTHER: "DİĞERLERİ",
+};
+
+const createToysTypesHtml = () => {
+	const filterEl = document.querySelector("#filter");
+	let filterHtml = "";
+	let filterTypes = ["ALL"];
+	toyList.forEach((toy) => {
+		if (filterTypes.findIndex((filter) => filter == toy.type) == -1) {
+			filterTypes.push(toy.type);
+		}
+	});
+
+	filterTypes.forEach((type, index) => {
+		filterHtml += `<li class="${
+			index == 0 ? "active" : null
+		}" onclick="filterToy(this)" data-type="${type}" > ${
+			TOYS_TYPES[type] || type
+		} </li>`;
+	});
+
+	filterEl.innerHTML = filterHtml;
+};
+
+const filterToy = (filterEl) => {
+	document.querySelector("#filter .active").classList.remove("active");
+	filterEl.classList.add("active");
+	let toyType = filterEl.dataset.type;
+	getToys();
+	if (toyType != "ALL") toyList = toyList.filter((toy) => toy.type == toyType);
+	createToyItemsHtml();
+};
+
 setTimeout(() => {
 	createToyItemsHtml();
-}, 500);
+	createToysTypesHtml();
+}, 300);
